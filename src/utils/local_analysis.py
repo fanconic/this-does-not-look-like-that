12b10f79827523a5c2984c355214e6f976fe2473
fanconic <fanconic@student.ethz.ch>
2020-12-30 18:56:33 +0100

    Added jpeg visualization emthod to local analysis

--- src/utils/local_analysis.py
+++ src/utils/local_analysis.py
@@ -1,5 +1,6 @@
 import torch
 import torchvision.transforms as transforms
+from torch.autograd import Variable
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
@@ -75,12 +76,17 @@ def imsave_with_bbox(fname, img_rgb, bbox_height_start, bbox_height_end,
 
 
 class LocalAnalysis(object):
-    def __init__(self, load_model_dir, load_model_name, test_image_name, attack=None):
+    def __init__(self, load_model_dir, load_model_name, test_image_name, image_save_directory = None, attack=None):
         model_base_architecture = load_model_dir.split('/')[-3]
         experiment_run = load_model_dir.split('/')[-2]
-
+        
+        
         self.save_analysis_path = '/cluster/scratch/{}/PPNet/local_analysis_attack{}/'.format(username, attack) \
                                     + model_base_architecture + '-' + experiment_run + '/'  + test_image_name[:-4]
+        
+        if image_save_directory is not None:
+            self.save_analysis_path = image_save_directory + test_image_name
+
         makedir(self.save_analysis_path)
 
         self.log, self.logclose = create_logger(log_filename=os.path.join(self.save_analysis_path, 'local_analysis.log'))
@@ -378,3 +384,176 @@ class LocalAnalysis(object):
         visualize_image_grid(torch2numpy, images=[images_test_unprocessed, images_perturbed, pert*127+0.5], 
                              titles=['Original Image', 'Perturbed Image', 'Perturbation'])
         return images_perturbed, pert
+
+
+    def jpeg_visualization(self, pil_img, img_name, test_image_label, preprocess_clean, preprocess_compressed, 
+                       show_images=False, idx=0, top_n = 1):
+        '''
+        Perform local analysis comparing compressed and uncompressed images.
+        Args:
+            pil_img: is the PIL test image which is to be inspected
+            img_name: name of the image to save it
+            test_image_label: label of the test image
+            preprocess_clean: first torch vision transform pipeline (here, without compression)
+            preprocess_compressed: second torch vision transform pipeline (here, with compression)
+            show_images (default = False): Boolean value to show images
+            idx (default = 0): Index Value, when retrieving results of the PPNet
+            top_n (default = 1): Visualize the n_th most activating prototype
+
+        Returns:
+            A list containing 8 images in the following order:
+                1. Full picture of most activated prototype with bounding box
+                2. Most activated prototype of compressed image
+                3. Compressed image passed through, with activated patch in bounding box
+                4. Corresponding activation map of the compressed image
+
+                5. Full picture of most activated prototype with bounding box
+                6. Most activated prototype of compressed image
+                7. Uncompressed image passed through, with activated patch in bounding box
+                8. Corresponding activation map of the uncompressed image
+        '''
+        # How to save the images
+        specific_folder = self.save_analysis_path + "/" + img_name
+        makedir(specific_folder)
+
+        # Preprocess Clean image
+        img_tensor_clean = preprocess_clean(pil_img)
+        img_variable_clean = Variable(img_tensor_clean.unsqueeze(0))
+
+        # Preprocess compressed image
+        img_tensor_compressed = preprocess_compressed(pil_img)
+        img_variable_compressed = Variable(img_tensor_compressed.unsqueeze(0))
+        
+        # Initialize as none, and will be filled after examining the first image
+        inspected_index = None
+        inspected_min = None
+        inspected_max = None
+
+        img_variables =[img_variable_compressed, img_variable_clean]
+
+        display_images = []
+        for img_variable in img_variables:
+
+            # Forward the image variable through the network
+            images_test = img_variable.cuda()
+            labels_test = torch.tensor([test_image_label])
+
+            logits, min_distances = self.ppnet_multi(images_test)
+            conv_output, distances = self.ppnet.push_forward(images_test)
+            prototype_activations = self.ppnet.distance_2_similarity(min_distances)
+            prototype_activation_patterns = self.ppnet.distance_2_similarity(distances)
+            if self.ppnet.prototype_activation_function == 'linear':
+                prototype_activations = prototype_activations + max_dist
+                prototype_activation_patterns = prototype_activation_patterns + max_dist
+
+            tables = []
+            for i in range(logits.size(0)):
+                tables.append((torch.argmax(logits, dim=1)[i].item(), labels_test[i].item()))
+
+
+            idx = idx
+            predicted_cls = tables[idx][0]
+            correct_cls = tables[idx][1]
+            if predicted_cls == correct_cls:
+                pred_text = 'Prediction is correct.'
+            else:
+                pred_text = 'Prediction is wrong.'
+            self.log('Predicted: ' + str(predicted_cls) + '\t Actual: '+ str(correct_cls) + '\t ' + pred_text)
+            
+            original_img = save_preprocessed_img(os.path.join(specific_folder, 'original_img.png'),
+                                                images_test, idx)
+
+            ##### MOST ACTIVATED (NEAREST) 10 PROTOTYPES OF THIS IMAGE
+            makedir(os.path.join(specific_folder, 'most_activated_prototypes'))
+            array_act, sorted_indices_act = torch.sort(prototype_activations[idx])
+
+            i=top_n
+            
+            if inspected_index is None:
+                inspected_index = sorted_indices_act[-i].item()
+
+            self.log("protoype index: " + str(inspected_index))
+
+            p_img = save_prototype(self.load_img_dir, os.path.join(self.save_analysis_path, 'most_activated_prototypes', 
+                                                'top-%d_activated_prototype.png' % i), 
+                                   self.start_epoch_number, inspected_index)
+
+            p_oimg_with_bbox = save_prototype_original_img_with_bbox(
+                self.load_img_dir,
+                fname=os.path.join(self.save_analysis_path, 'most_activated_prototypes', 
+                                   'top-%d_activated_prototype_in_original_pimg.png' % i), 
+                epoch=self.start_epoch_number,
+                index=inspected_index,
+                bbox_height_start=self.prototype_info[inspected_index][1],
+                bbox_height_end=self.prototype_info[inspected_index][2],
+                bbox_width_start=self.prototype_info[inspected_index][3],
+                bbox_width_end=self.prototype_info[inspected_index][4],
+                color=(0, 255, 255))
+            p_img_with_self_actn = save_prototype_self_activation(
+                self.load_img_dir,
+                os.path.join(self.save_analysis_path, 'most_activated_prototypes', 
+                             'top-%d_activated_prototype_self_act.png' % i), 
+                self.start_epoch_number, inspected_index)
+        
+            self.log('prototype class identity: {0}'.format(self.prototype_img_identity[inspected_index]))
+            if self.prototype_max_connection[inspected_index] != self.prototype_img_identity[sorted_indices_act[-i].item()]:
+                self.log('prototype connection identity: {0}'.format(self.prototype_max_connection[inspected_index]))
+            self.log('activation value (similarity score): {0}'.format(prototype_activations[idx][inspected_index]))
+
+            activation_pattern = prototype_activation_patterns[idx][inspected_index].detach().cpu().numpy()
+            upsampled_activation_pattern = cv2.resize(activation_pattern, dsize=(self.img_size, self.img_size),
+                                                    interpolation=cv2.INTER_CUBIC)
+
+            # show the most highly activated patch of the image by this prototype
+            high_act_patch_indices = find_high_activation_crop(upsampled_activation_pattern)
+            high_act_patch = original_img[high_act_patch_indices[0]:high_act_patch_indices[1],
+                                        high_act_patch_indices[2]:high_act_patch_indices[3], :]
+            
+            plt.imsave(os.path.join(specific_folder, 'most_activated_prototypes',
+                                    'most_highly_activated_patch_by_top-%d_prototype.png' % i),
+                    high_act_patch)
+            
+            p_img_with_bbox = imsave_with_bbox(
+                fname=os.path.join(specific_folder, 'most_activated_prototypes',
+                                'most_highly_activated_patch_in_original_img_by_top-%d_prototype.png' % i), 
+                img_rgb=original_img,
+                bbox_height_start=high_act_patch_indices[0],
+                bbox_height_end=high_act_patch_indices[1],
+                bbox_width_start=high_act_patch_indices[2],
+                bbox_width_end=high_act_patch_indices[3], color=(0, 255, 255))
+
+            # show the image overlayed with prototype activation map and use normalization values of first run
+            if inspected_min is None:
+                inspected_min = np.amin(upsampled_activation_pattern)
+
+            rescaled_activation_pattern = upsampled_activation_pattern - inspected_min
+
+            if inspected_max is None:
+                inspected_max = np.amax(rescaled_activation_pattern)
+                
+            rescaled_activation_pattern = rescaled_activation_pattern / inspected_max
+            heatmap = cv2.applyColorMap(np.uint8(255*rescaled_activation_pattern), cv2.COLORMAP_JET)
+            heatmap = np.float32(heatmap) / 255
+            heatmap = heatmap[...,::-1]
+            overlayed_img = 0.5 * original_img + 0.3 * heatmap
+    
+            plt.imsave(os.path.join(specific_folder, 'most_activated_prototypes',
+                                    'prototype_activation_map_by_top-%d_prototype.png' % i),
+                    overlayed_img)
+            
+            display_images += [p_oimg_with_bbox, p_img, p_img_with_bbox, overlayed_img]
+            self.log("------------------------------")
+            
+        # Visualize Images
+        if show_images:
+            display_titles = ['Training Image from which \nprototype is taken', 'Prototype', 
+                                        'Test Image + BBox', 'Test Image + Activation Map']
+
+            display_titles += [x + "\n(uncompressed)" for x in display_titles]
+
+            visualize_image_grid(images=display_images, 
+                                titles= display_titles, ncols=8)
+            plt.tight_layout()
+            plt.show()
+
+        return display_images
